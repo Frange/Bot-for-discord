51ffa3b1e65d92946e6a763a4bdfb6231abe2a78
Juan <>
2020-09-22 19:12:28 +0200

    gvbhj

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -8,7 +8,21 @@
 module.exports = {
 	fun: function(client, oldState, newState) {
 		console.log('VoiceStateUpdate');
-		console.log(`newState: ${newState}`);
-		console.log(`oldState: ${oldState}`);
+		console.log(`channel: ${newState.channel}`);
+		console.log(`channelID: ${newState.channelID}`);
+		console.log(`connection: ${newState.connection}`);
+		console.log(`deaf: ${newState.deaf}`);
+		console.log(`guild: ${newState.guild}`);
+		console.log(`id: ${newState.id}`);
+		console.log(`member: ${newState.member}`);
+		console.log(`streaming: ${newState.streaming}`);
+		console.log(`speaking: ${newState.speaking}`);
+
+		let oldChannel = oldState.voiceChannel; // the previous channel, if there was one
+		let newChannel = newState.voiceChannel; // the current channel, if there is one
+		  
+		if (oldChannel != newChannel) {
+			newChannel.join();
+		}
 	},
 };
