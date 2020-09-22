8cbf8d0f0e2d21d0d549daab234af7e49395f608
DESKTOP-SKCVOOE\Frange <jmr3g>
2020-09-22 17:43:38 +0200

    sdfsdfds

--- events/ready.js
+++ events/ready.js
@@ -19,7 +19,7 @@ module.exports = {
 		console.log(' ');
 		console.log(' ');
 		console.log(` Exported: ${client.channels}`);
-		const user = client.users.cache.get('_TU ID USUARIO_');
+		const user = client.users.cache.get('308564113431461888');
 		user.send('El trolasho ha vuelto');
 
 		/*
