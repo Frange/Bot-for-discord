059de35717c1701d090686a802d78c24893b35f4
Juan M <>
2020-09-23 16:29:07 +0200

    sfdfdssd

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -44,13 +44,12 @@ async function mySend(client, userId) {
 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.arc(225, 225, 100, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
 
 	const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'jpg' }));
 	ctx.drawImage(avatar, 25, 25, 200, 200);
-	ctx.lineTo(avatar, 50, 50);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'newChallenger.png');
 
