5b0278bd1dd8969b2a30779c4d54c69e74915707
Juan M <>
2020-09-23 15:44:25 +0200

    dgffddgf

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -28,7 +28,7 @@ const CHANNEL_FALL_GUYS = '750723648100237363';
 const VOICE_CHANNEL_AMONG_US = '753022620298903645';
 const CHANNEL_AMONG_US = '753022463155372193';
 
-async function mySend() {
+async function mySend(user) {
 	const canvas = Canvas.createCanvas(854, 480);
 	const ctx = canvas.getContext('2d');
 
@@ -43,7 +43,7 @@ async function mySend() {
 	ctx.closePath();
 	ctx.clip();
 
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'jpg' }));
 	ctx.drawImage(avatar, 600, 150, 200, 200);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'newChallenger.png');
@@ -63,11 +63,11 @@ module.exports = {
 			if (newState.channelID === VOICE_CHANNEL_FALL_GUYS) {
 				console.log(' ');
 				console.log(`VoiceStateUpdate - ${userName} se ha conecado a ${channel}`);
-				client.channels.cache.get(CHANNEL_FALL_GUYS).send(mySend());
+				client.channels.cache.get(CHANNEL_FALL_GUYS).send(mySend(newState));
 			} else if (newState.channelID === VOICE_CHANNEL_AMONG_US) {
 				console.log(' ');
 				console.log(`VoiceStateUpdate - ${userName} se ha conecado a ${channel}`);
-				client.channels.cache.get(CHANNEL_AMONG_US).send(mySend());
+				client.channels.cache.get(CHANNEL_AMONG_US).send(mySend(newState));
 			}
 		}
 	},
