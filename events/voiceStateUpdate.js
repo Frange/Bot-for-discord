41f5e86bbda90c5193f19043311a75a4d9bc5b4f
Juan M <>
2020-09-24 15:25:54 +0200

    cfvgbh

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -48,11 +48,11 @@ async function mySend(client, userId) {
 	console.log('1');
 
 	const user = client.users.cache.get(userId);
-	const canvas = Canvas.createCanvas(images1p[randomNumber].xsize, images1p[randomNumber].ysize);
+	const canvas = Canvas.createCanvas(images[randomNumber].xsize, images[randomNumber].ysize);
 	const ctx = canvas.getContext('2d');
 
 	console.log('2');
-	const background = await Canvas.loadImage(images1p[randomNumber].img);
+	const background = await Canvas.loadImage(images[randomNumber].img);
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	console.log('3');
@@ -61,9 +61,9 @@ async function mySend(client, userId) {
 
 	console.log('4');
 	// Modify this 3 parameters to change size and position of the avatar image.
-	const xCenter = images1p[randomNumber].x; // Set X position of center circle.
-	const yCenter = images1p[randomNumber].y; // Set Y position of center circle.
-	const radious = images1p[randomNumber].rad; // Avatar size = radious * 2.
+	const xCenter = images[randomNumber].x; // Set X position of center circle.
+	const yCenter = images[randomNumber].y; // Set Y position of center circle.
+	const radious = images[randomNumber].rad; // Avatar size = radious * 2.
 
 	console.log('5');
 	ctx.beginPath();
