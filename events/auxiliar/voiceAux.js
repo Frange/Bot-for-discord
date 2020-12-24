796399a285f5eda3bb2301c30f641f75f2685bb3
DESKTOP-SKCVOOE\Frange <jmr3g>
2020-12-24 12:48:30 +0100

    Refactor almost finished

--- events/auxiliar/voiceAux.js
+++ events/auxiliar/voiceAux.js
@@ -1,3 +1,6 @@
+/* eslint-disable brace-style */
+/* eslint-disable no-inline-comments */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 
@@ -23,6 +26,156 @@ function showLogs(oldState, newState) {
 	*/
 }
 
+async function mySend(constants, voiceAux, client, userId, channel) {
+	const randomNumber = 1;
+	// let images = images3p;
+	let size = 0; position = 1;
+
+	const user = client.users.cache.get(userId);
+
+	const canvas = constants.canvas.createCanvas(
+		constants.images3p[randomNumber].xsize,
+		constants.images3p[randomNumber].ysize);
+	const ctx = canvas.getContext('2d');
+
+	const background = await constants.canvas.loadImage(constants.images3p[randomNumber].img);
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	for (const [memberID, member] of channel.members) {
+		size++;
+	}
+
+	console.log(`Size: ${size}`);
+	console.log(' ');
+
+	/*
+	switch (size) {
+		case 1: {
+			images = images1p;
+			break;
+		}
+		case 2: {
+			images = images2p;
+			break;
+		}
+		case 3: {
+			images = images3p;
+			break;
+		}
+		case 4: {
+			images = images4p;
+			break;
+		}
+	}
+	*/
+
+	console.log(' frg 1');
+	voiceAux.renderAvatar(user, position, constants.images3p, ctx);
+	console.log(' frg 2');
+
+	/*
+	for (const [memberID, member] of channel.members) {
+		console.log(`MemberID: ${memberID}`);
+		console.log(`userId: ${userId}`);
+		console.log(`user: ${user}`);
+		// if (memberID != userId) {
+			position++;
+			console.log(`jm 1 `);
+			user = client.users.cache.get(memberID);
+
+			console.log(`jm 2 `);
+			renderAvatar(user, position, images3p, ctx);
+			console.log(`jm 3 `);
+		// }
+	}
+	*/
+
+	const attachment = new constants.Discord.MessageAttachment(canvas.toBuffer(), 'newChallenger.png');
+
+	// client.users.cache.get('308564113431461888').send(`Hay un nuevo contrincante ${user.username}`, attachment);
+	client.channels.cache.get(constants.CHANNEL_JM).send(`Hay un nuevo contrincante ${user.username}`, attachment);
+	// canal.send(`Hay un nuevo contrincante ${member}`, attachment);
+}
+
+async function mySend2(constants, client, userId) {
+	const user = client.users.cache.get(userId);
+
+	const canvas = constants.canvas.createCanvas(constants.images3p[0].xsize, constants.images3p[0].ysize);
+	const ctx = canvas.getContext('2d');
+
+	const background = await constants.canvas.loadImage(constants.images3p[0].img);
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Modify this 3 parameters to change size and position of the avatar image.
+	const xCenter = constants.images3p[0].x1; // Set X position of center circle.
+	const yCenter = constants.images3p[0].y2; // Set Y position of center circle.
+	const radious = constants.images3p[0].rad; // Avatar size = radious * 2.
+
+	ctx.beginPath();
+	ctx.arc(xCenter, yCenter, radious, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await constants.canvas.loadImage(user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, (xCenter - radious), (yCenter - radious), (radious * 2), (radious * 2));
+
+	const attachment = new constants.Discord.MessageAttachment(canvas.toBuffer(), 'newChallenger.png');
+
+	// client.users.cache.get('308564113431461888').send(`Hay un nuevo contrincante ${user.username}`, attachment);
+	client.channels.cache.get(constants.CHANNEL_JM).send(`Hay un nuevo contrincante ${user.username}`, attachment);
+	// canal.send(`Hay un nuevo contrincante ${member}`, attachment);
+}
+
+function removeMessages(client, channel) {
+	console.log(`channel: ${channel}`);
+}
+
 function setMuteAll(client, channel, isMute) {
 	console.log(`channel: ${channel}`);
+}
+
+function muteForAmongUs(constants, client, oldState, newState) {
+	if (oldState != null && oldState.channelID != null) {
+		const oUserName = client.users.cache.find((u) => u.id === oldState.id).username;
+		const oChannelId = oldState.channelID;
+
+		if (oChannelId === constants.VOICE_CHANNEL_AMONG_US) {
+			console.log(' AMONG US');
+
+			console.log(`OldState: oldState.id: @${oldState.id}`);
+			console.log(`OldState: USER_FRANGE: @${constants.USER_FRANGE}`);
+
+			console.log(`OldState: Mute: @${oldState.mute}`);
+			console.log(`OldState: SelfMute: @${oldState.selfMute}`);
+			console.log(`OldState: Speaking: @${oldState.speaking}`);
+
+			if (oldState.id === constants.USER_FRANGE) {
+				if (oldState.mute) {
+					// ANTES ESTABA MUTEADO
+					setMuteAll(client, oChannelId, false);
+					if (oldState.selfMute) {
+						console.log(' ANTES ESTABA MUTEADO Y SELF MUTEADO');
+						// muteAll(true);
+					} else {
+						console.log(' ANTES ESTABA MUTEADO Y NO ESTABA SELF MUTEADO');
+						// mkmkmkmkmuteAll(false);
+					}
+				} else {
+					console.log(' ANTES ESTABA NO MUTEADO');
+					setMuteAll(true);
+
+					if (oldState.selfMute) {
+						console.log(' ANTES ESTABA SELF MUTEADO');
+					} else {
+						console.log(' ANTES NO ESTABA SELF MUTEADO');
+					}
+				}
+			}
+		}
+	}
 }
