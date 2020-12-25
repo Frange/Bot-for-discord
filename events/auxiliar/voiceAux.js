7d227d3efdac5ebd3324c34edcec563fdab85960
DESKTOP-SKCVOOE\Frange <jmr3g>
2020-12-25 11:00:33 +0100

    fgffg

--- events/auxiliar/voiceAux.js
+++ events/auxiliar/voiceAux.js
@@ -1,26 +1,32 @@
+/* eslint-disable no-empty */
 /* eslint-disable brace-style */
 /* eslint-disable no-inline-comments */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
 function showLogs(oldState, newState) {
-	console.log(`OldState: Id: @${oldState.id}`);
-	console.log(`OldState: Connection: @${oldState.connection}`);
-	console.log(`OldState: Member: @${oldState.member}`);
-	console.log(`OldState: Mude: @${oldState.mute}`);
-	console.log(`OldState: SelfMute: @${oldState.selfMute}`);
-	console.log(`OldState: Speaking: @${oldState.speaking}`);
-	console.log(`OldState: channel: @${oldState.channel}`);
-	console.log(`OldState: channelID: @${oldState.channelID}`);
-	console.log(' ');
-	console.log(`NewState: Id: @${newState.id}`);
-	console.log(`NewState: Connection: @${newState.connection}`);
-	console.log(`NewState: Member: @${newState.member}`);
-	console.log(`NewState: Mute: @${newState.mute}`);
-	console.log(`NewState: SelfMute: @${newState.selfMute}`);
-	console.log(`NewState: Speaking: @${newState.speaking}`);
-	console.log(`NewState: Channel: @${newState.channel}`);
-	console.log(`NewState: ChannelID: @${newState.channelID}`);
+	try {
+		console.log(`OldState: Id: @${oldState.id}`);
+		console.log(`OldState: Connection: @${oldState.connection}`);
+		console.log(`OldState: Member: @${oldState.member}`);
+		console.log(`OldState: Mude: @${oldState.mute}`);
+		console.log(`OldState: SelfMute: @${oldState.selfMute}`);
+		console.log(`OldState: Speaking: @${oldState.speaking}`);
+		console.log(`OldState: channel: @${oldState.channel}`);
+		console.log(`OldState: channelID: @${oldState.channelID}`);
+		console.log(' ');
+		console.log(`NewState: Id: @${newState.id}`);
+		console.log(`NewState: Connection: @${newState.connection}`);
+		console.log(`NewState: Member: @${newState.member}`);
+		console.log(`NewState: Mute: @${newState.mute}`);
+		console.log(`NewState: SelfMute: @${newState.selfMute}`);
+		console.log(`NewState: Speaking: @${newState.speaking}`);
+		console.log(`NewState: Channel: @${newState.channel}`);
+		console.log(`NewState: ChannelID: @${newState.channelID}`);
+	} catch (ex) {
+
+	}
+
 }
 
 async function mySend(constants, voiceAux, client, userId, channel) {
