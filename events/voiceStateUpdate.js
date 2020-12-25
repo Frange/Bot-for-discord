8be8ac5fb05baeb0b930104a3636f2f5840be40a
DESKTOP-SKCVOOE\Frange <jmr3g>
2020-12-25 10:21:23 +0100

    dhdggdh

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -15,6 +15,7 @@
 module.exports = {
 	fun: function(constants, voiceAux, client, oldState, newState) {
 
+		console.log(`voiceAux: @${voiceAux}`);
 		voiceAux.showLogs(oldState, newState);
 
 		voiceAux.muteForAmongUs(constants, client, oldState, newState);
